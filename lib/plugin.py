d8e74ff812dfcc8df89ec6d815029d941a298275
ph4ntome@gmail.com <ph4ntome@gmail.com@81c8bb96-aa45-f2e2-0eef-c4fa4a15c6df>
2014-10-31 14:15:46 +0000

    Fixed some things, now it partialy works What doesn't work is --list-[plugin]
    git-svn-id: https://callirhoe.googlecode.com/svn/branches/phantome@175 81c8bb96-aa45-f2e2-0eef-c4fa4a15c6df

--- lib/plugin.py
+++ lib/plugin.py
@@ -43,7 +43,7 @@ def available_files(parent, dir, fmatch = None):
     good = False
     res = []
     pattern = parent + "/" + dir + "/*.py"
-    for x in glob.glob(pattern) if parent != "resource" else resources.resource_list[dir]:
+    for x in glob.glob(pattern) if parent != "/" else resources.resource_list[dir]:
         basex = os.path.basename(x)
         if basex == "__init__.py": good = True
         elif basex.startswith('_'):
@@ -77,7 +77,7 @@ def get_plugin_paths():
     result = [ os.path.expanduser("~/.callirhoe"), sys.path[0] if sys.path[0] else "." ]
     try:
         temp = resources.resource_list
-        result.append("resource")
+        result.append("/")
     except:
         pass
     return result
